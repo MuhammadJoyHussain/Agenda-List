d126ba2497effe9ee172271a9fbf7e8fdce7aaee
Muhammad Joynul Hussain <muhammadjoyhussain@gmail.com>
2021-12-20 19:52:02 +0600

    Agenda import/export completed

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        id: 1,
+        name: 'joynul',
+        age: 24
+    },
+    {
+        id: 2,
+        name: 'jaynul',
+        age: 24
+    },
+    {
+        id: 3,
+        name: 'joynal',
+        age: 24
+    },
+]
+
+export default data;
